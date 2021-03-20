b47ffa5fd45105e4d39644ec8651c1783a5d13cd
YukiS-27 <yukis.job27@gmail.com>
2021-03-20 19:45:29 +0900

    ユーザーページ変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: "sound_posts#index"
   get 'home/index'
   get 'home/show'
-  
+
   # deviseのコントローラーを継承
   devise_for :users, :controllers => {
     :confirmations => 'users/confirmations',
@@ -12,15 +12,15 @@ Rails.application.routes.draw do
     :sessions => 'users/sessions',
     :passwords => 'users/passwords'
   }
-  
+
   devise_scope :user do
     get "sign_in", :to => "users/sessions#new"
     get "sign_out", :to => "users/sessions#destroy"
   end
-  
+
   get 'users/index', to: 'profiles#index', as: 'users'
   get 'users/:id', to: 'profiles#show', as: 'detail_user'
 
   resources :sound_posts
-  
+
 end
