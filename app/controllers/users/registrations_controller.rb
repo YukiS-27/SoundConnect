e6bab91a87b7bb8450dfb676eceb15d5a291493f
YukiS-27 <yukis.job27@gmail.com>
2021-03-22 14:41:17 +0900

    プロフィール編集の修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -63,13 +63,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
     # def configure_account_update_params
-      # 情報更新時にnameの取得を許可する
-      # devise_parameter_sanitizer.permit(:account_update, keys: [:name, :introduction])
+        # 情報更新時にnameの取得を許可する
+    #   devise_parameter_sanitizer.permit(:account_update, keys: [:name, :introduction])
     # end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
 
   # The path used after sign up.
