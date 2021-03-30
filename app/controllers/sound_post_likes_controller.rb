08ee39752f8e00e468ef1bae0d0c242a83132584
YukiS-27 <yukis.job27@gmail.com>
2021-03-30 10:54:20 +0900

    いいね機能の実装

--- app/controllers/sound_post_likes_controller.rb
+++ app/controllers/sound_post_likes_controller.rb
@@ -1,2 +1,19 @@
 class SoundPostLikesController < ApplicationController
+  def create
+    SoundPostLike.create(
+      user_id: current_user.id,
+      sound_post_id: params[:sound_post_id]
+    )
+    # 一旦トップページへ
+    redirect_to root_path
+  end
+
+  def destroy
+    sound_post_like = SoundPostLike.find_by(
+      user_id: current_user.id,
+      sound_post_id: params[:id]
+    )
+    sound_post_like.destroy if sound_post_like.present?
+    redirect_to root_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,12 @@ class User < ApplicationRecord
   has_many :sound_posts
 
   has_many :sound_post_likes
-  has_many :sound_post_like_users, through: :sound_post_likes, source: :sound_post
+  has_many :like_sound_posts, through: :sound_post_likes, source: :sound_post
 
   has_many :playlists
   has_many :playlist_sound_posts, through: :playlists, source: :sound_post
 
+  def liked_by?(sound_post)
+    sound_post_likes.where(sound_post_id: sound_post.id).exists?
+  end
 end
