519fadd545701b4e271c2eea023108bfc3e64894
YukiS-27 <yukis.job27@gmail.com>
2021-04-11 01:35:37 +0900

    Userモデルからprofile関連のカラム削除

--- db/migrate/20210410154931_create_profiles.rb
+++ db/migrate/20210410154931_create_profiles.rb
@@ -1,6 +1,10 @@
 class CreateProfiles < ActiveRecord::Migration[6.1]
   def change
     create_table :profiles do |t|
+      t.string :name, null: false
+      t.text :introduction, null: false
+      t.string :avatar
+      t.string :genre
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_095832) do
+ActiveRecord::Schema.define(version: 2021_04_10_163118) do
 
   create_table "instruments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2021_03_30_095832) do
   create_table "playlists", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "profiles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "introduction", null: false
+    t.string "avatar"
+    t.string "genre"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -62,10 +71,6 @@ ActiveRecord::Schema.define(version: 2021_03_30_095832) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.text "introduction"
-    t.string "avatar"
-    t.string "genre"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
