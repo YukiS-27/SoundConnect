530e49b6fc5a9917259748fb9e035732f39c8800
YukiS-27 <yukis.job27@gmail.com>
2021-04-11 02:16:02 +0900

    Userモデルにprofile_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_163118) do
+ActiveRecord::Schema.define(version: 2021_04_10_170823) do
 
   create_table "instruments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -71,10 +71,13 @@ ActiveRecord::Schema.define(version: 2021_04_10_163118) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "profile_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["profile_id"], name: "index_users_on_profile_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "sound_post_likes", "sound_posts"
   add_foreign_key "sound_post_likes", "users"
+  add_foreign_key "users", "profiles"
 end
