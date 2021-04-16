06c941ca35fa0941e405e82dba0b160276a7ab09
YukiS-27 <yukis.job27@gmail.com>
2021-04-16 15:34:11 +0900

    messageモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_165423) do
+ActiveRecord::Schema.define(version: 2021_04_16_062433) do
 
   create_table "instruments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -20,10 +20,12 @@ ActiveRecord::Schema.define(version: 2021_04_15_165423) do
 
   create_table "messages", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
     t.text "content", null: false
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
@@ -100,6 +102,7 @@ ActiveRecord::Schema.define(version: 2021_04_15_165423) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
   add_foreign_key "profiles", "users"
   add_foreign_key "room_users", "rooms"
